546ba008041bce3cc7b683f85769b7ec8cec388e
manasapala <33602592+manasapala@users.noreply.github.com>
2020-11-03 18:10:42 -0800

    SNS notification for different scenarios

--- trigger-multiplier.py
+++ trigger-multiplier.py
@@ -5,6 +5,16 @@ from botocore.exceptions import ClientError
 
 BUCKET_NAME = 'demos-s3-lambda'
 
+def snsNotify(errorCode):
+    snsTopicArn = 'arn:aws:sns:us-east-2:408150979308:build-phase-topic'
+    subject = "Build phase trigger status"
+    messageBody =  errorCode 
+    client = boto3.client('sns')
+    client.publish(
+            TopicArn = snsTopicArn,
+            Message = messageBody,
+            Subject = subject
+    )
 
 def download_file(BUCKET_NAME, ssmKey, ssmValue): 
     s3 = boto3.resource('s3')
@@ -16,20 +26,29 @@ def download_file(BUCKET_NAME, ssmKey, ssmValue):
         else:
             folderName = 'windows'    
         filesResponse = s3Client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=folderName)
-        for s3_object in filesResponse['Contents']:
-            if s3_object['Key'].endswith(("config.json", "_files")): 
-                filename = s3_object['Key']
-                s3object = s3.Object(BUCKET_NAME, filename)   
-                body = s3object.get()['Body'].read()
-                configData = json.loads(body)
+        if filesResponse['KeyCount'] > 0:
+            configFileCount = 0    
+            for s3_object in filesResponse['Contents']:
+                if s3_object['Key'].endswith(("config.json", "_files")): 
+                    filename = s3_object['Key']
+                    s3object = s3.Object(BUCKET_NAME, filename)   
+                    body = s3object.get()['Body'].read()
+                    configData = json.loads(body)
 
-                for amiConfig in configData['regionConfig']:                    
-                    amiConfig['amiConfig']['amiId'] = ssmValue
+                    for amiConfig in configData['regionConfig']:                    
+                        amiConfig['amiConfig']['amiId'] = ssmValue
 
-                s3object.put(
-                    Body=(bytes(json.dumps(configData).encode('UTF-8')))
-                )    
+                    s3object.put(
+                        Body=(bytes(json.dumps(configData).encode('UTF-8')))
+                    ) 
+            configFileCount += 1
+            if configFileCount > 0:
+                snsNotify('trigger.py lambda has completed and updated'+ ' ' + str(configFileCount) + ' '+ 'config files')         
+        else:
+            snsNotify('The folder' + ' ' + folderName +' '+'is not found or config files doesnot exist in this folder')              
     except ClientError as e:
+        err = e.response
+        snsNotify('The S3 bucket'+' '+ BUCKET_NAME + ' ' + 'is not found or cannot be accessed and the error code is:'+ ' ' +str(err))
         return False
 
 
